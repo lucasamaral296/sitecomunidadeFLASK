e6d9184de30ca91e2e6196cc232ed267586b5f06
Lucas Velasque <lucas.amaralvelasque@hotmail.com>
2023-06-14 17:12:21 -0300

    criando pag html

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Meu site esta no ar!</p>"
+def home():
+    return render_template('home.html')
 
 @app.route('/contato')
 def contato():
-    return 'Qualquer duvida entre em contato'
+    return render_template('contato.html')
 
 
 if __name__ == "__main__":
