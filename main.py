141d1fe18bfaa6075c6c986c56766f0e7d22756f
Lucas Velasque <lucas.amaralvelasque@hotmail.com>
2023-06-14 16:38:19 -0300

    incluindo uma pagina no flask

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Meu site esta no ar!</p>"
+
+@app.route('/contato')
+def contato():
+    return 'Qualquer duvida entre em contato'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
