bb9306fd2350200b6b58f218b92da358e806435a
Lucas Velasque <lucas.amaralvelasque@hotmail.com>
2023-06-14 18:43:28 -0300

    variaveis python no html

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
+
 @app.route("/")
 def home():
     return render_template('home.html')
@@ -9,6 +11,10 @@ def home():
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
 
 if __name__ == "__main__":
