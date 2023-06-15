e366aff0de78a8e264afb71ce244b9de184016cb
Lucas Velasque <lucas.amaralvelasque@hotmail.com>
2023-06-15 11:58:25 -0300

    adicionando flash messages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
@@ -7,23 +7,37 @@ lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
 
 app.config['SECRET_KEY'] = '6037c3d29cc427f4051a2521ea3adf25168c7b44057fa953'
 
+
 @app.route("/")
 def home():
     return render_template('home.html')
+
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
 
+
 @app.route('/usuarios')
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        # exibir msg de 'login' com sucesso
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
+        # redirecionar para a homepage
+        return redirect(url_for('home'))
+    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        # criou conta com sucesso
+        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
+        return redirect(url_for('home'))
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
