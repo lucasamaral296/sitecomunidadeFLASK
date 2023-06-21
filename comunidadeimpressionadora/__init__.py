d51e47aab4a21f002b789617537a3fdfa0d579f9
Lucas Velasque <lucas.amaralvelasque@hotmail.com>
2023-06-21 18:12:58 -0300

    redirecionamento para login

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -13,6 +13,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Por favor faça login para acessar essa página'
+login_manager.login_message_category = 'alert-info'
 
 
 
--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, request, redirect, flash, url_for
 from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta
 from comunidadeimpressionadora.models import Usuario
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, current_user, login_required
 
 lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
 
@@ -18,6 +18,7 @@ def contato():
 
 
 @app.route('/usuarios')
+@login_required
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
@@ -32,8 +33,12 @@ def login():
             login_user(usuario, remember=form_login.lembrar_dados.data)
             # exibir msg de 'login' com sucesso
             flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
-            # redirecionar para a homepage
-            return redirect(url_for('home'))
+            par_next = request.args.get('next')
+            if par_next:
+                return redirect(par_next)
+            else:
+                # redirecionar para a homepage
+                return redirect(url_for('home'))
         else:
             flash('Falha no Login, E-mail ou senha incorretos.', 'alert-danger')
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
@@ -50,6 +55,7 @@ def login():
 
 
 @app.route('/sair')
+@login_required
 def sair():
     logout_user()
     flash('Logout feito com sucesso.', 'alert-success')
@@ -57,11 +63,13 @@ def sair():
 
 
 @app.route('/perfil')
+@login_required
 def perfil():
     return render_template('perfil.html')
 
 
 @app.route('/post/criar')
+@login_required
 def criar_post():
     return render_template('criarpost.html')
 
