24240d39d203da6a7b3a5c0e6cc5601517365af7
Lucas Amaral Velasque <81877529+lucasamaral296@users.noreply.github.com>
2023-07-04 19:56:07 -0300

    excluir post e popup de confirmação

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, flash, url_for
+from flask import render_template, request, redirect, flash, url_for, abort
 from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCriarPost
 from comunidadeimpressionadora.models import Usuario, Post
@@ -153,3 +153,16 @@ def exibir_post(post_id):
     else:
         form = None
     return render_template('post.html', post=post, form=form)
+
+
+@app.route('/post/<post_id>/excluir', methods=['GET', 'POST'])
+@login_required
+def excluir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        database.session.delete(post)
+        database.session.commit()
+        flash('Post Excluido com Sucesso', 'alert-danger')
+        return redirect(url_for('home'))
+    else:
+        abort(403)
