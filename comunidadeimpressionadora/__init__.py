94c6bdb0ffdf25912a6578c693c9d50950e2deff
Lucas Amaral Velasque <81877529+lucasamaral296@users.noreply.github.com>
2023-07-05 19:13:57 -0300

    ajustando para deploy banco de dados

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
+import sqlalchemy
 
 
 app = Flask(__name__)
 
-
 app.config['SECRET_KEY'] = '6037c3d29cc427f4051a2521ea3adf25168c7b44057fa953'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+if os.getenv("Internal Database URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("Internal Database URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -17,7 +21,16 @@ login_manager.login_view = 'login'
 login_manager.login_message = 'Por favor faça login para acessar essa página'
 login_manager.login_message_category = 'alert-info'
 
-
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de Dados criado")
+else:
+    print("Base de dados já criada")
 
 from comunidadeimpressionadora import routes
 
