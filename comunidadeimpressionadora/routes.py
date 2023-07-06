59b99f996e652585bb027a3c3db4d1481f45b915
Lucas Amaral Velasque <81877529+lucasamaral296@users.noreply.github.com>
2023-07-06 08:38:24 -0300

    ajuste deploy decode utf

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -48,7 +48,7 @@ def login():
         else:
             flash('Falha no Login, E-mail ou senha incorretos.', 'alert-danger')
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
-        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data).decode('utf-8')
         # criar Usuario
         usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data, senha=senha_cript)
         # adicionar a sessao
